f4b7651aedad1d9f3d7e4b9b4a89187953de243a
rajat121292 <rajat121292@gmail.com>
2019-09-01 14:25:00 +0530

    server.js contains the previous code, the new code is in index.js, use it run nodemon index.js

--- index.js
+++ index.js
@@ -3,129 +3,104 @@ var express = require('express');
 var app = express();
 var mysql      = require('mysql');
 var bodyParser = require('body-parser');
+const EventEmitter = require('events');
+
+
+const d = require('domain').create();
+d.on('error', (er) => {
+  // The error won't crash the process, but what it does is worse!
+  // Though we've prevented abrupt process restarting, we are leaking
+  // resources like crazy if this ever happens.
+  // This is no better than process.on('uncaughtException')!
+  console.log(`error, but oh well ${er.message}`);
+});
+
+d.run(() => {
+
 
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'iamreputed',
-  database : 'test',
-});
- 
- 
-connection.connect(function(err) {
-  if (err) throw err
-  console.log('You are now connected with mysql database...')
-})
-
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
-}));
-
-
-
-var server = app.listen(3000, "127.0.0.1", function () {
- 
-    var host = server.address().address
-    var port = server.address().port
-   
-    console.log("Example app listening at http://%s:%s", host, port)
-   
+    host     : 'localhost',
+    user     : 'root',
+    password : 'iamreputed',
+    database : 'test',
   });
-
- app.post('/user/add', function(req, res) {
-
-    // res.header("Access-Control-Allow-Origin", "*");
-    // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-
-     var params = req.body;
-     console.log(params);
-     connection.query('INSERT INTO user SET ?', params, function (error, results, fields) {
-         if (error) {
-             res.end(JSON.stringify(error));
-        }
-         res.end(JSON.stringify(results));
-     })
- })
-
- app.post('/asset', function(req, res) {
-     var params = req.body;
-     console.log(params);
-     connection.query('INSERT INTO asset SET ?', params, function(error, results, fields) {
-         if (error) {
-             res.end(JSON.stringify(error));
-             throw error;
+   
+   
+  connection.connect(function(err) {
+    if (err) throw err
+    console.log('You are now connected with mysql database...')
+  })
+  
+  connection.on('error', (err) => {
+      // If the connection is reset by the server, or if it can't
+      // connect at all, or on any sort of error encountered by
+      // the connection, the error will be sent here.
+      console.log("HAHA");
+    });
+  
+  
+  
+  app.use( bodyParser.json() );       // to support JSON-encoded bodies
+  app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+  }));
+  
+  
+  
+  var server = app.listen(3000, "127.0.0.1", function () {
+   
+      var host = server.address().address
+      var port = server.address().port
+     
+      console.log("Example app listening at http://%s:%s", host, port)
+     
+    });
+  
+//   process.on('uncaughtException', function (err) {
+//       console.error(err);
+//       console.log("Node NOT Exiting...");
+//     });
+  
+   app.post('/user', function(req, res) {
+  
+       var params = req.body;
+       console.log(params);
+       let queryParams = {
+           name: params.name,
+           id_proof: params.idProof,
+           id_proof_value: params.idProofValue,
+           email: params.email,
+           password: params.password
+       };
+  
+       connection.query('INSERT INTO user SET ?', queryParams, function (error, results, fields) {
+           if (error) {  
+                res.send(JSON.stringify(error));
+               throw error;
+          }
+           queryResult = results;
+       });
+       
+       connection.query('SELECT * FROM user where email = ?', [params.email], function (error, results, fields) {
+          if (error) {
+              throw error;
          }
+         
+          res.send(results);
+      });
+   })
+  
+   app.get('/user/email/:email/password/:password', function (req, res) {
+       var params = req.body;
+       var email = params.email;
+       var password = params.password;
+  
+      connection.query('select * from user where email=? and password=?', [email, password], function (error, results, fields) {
+         if (error) throw error;
          res.end(JSON.stringify(results));
-     })
- })
+       });
+   });
+  
+  
+});
 
- app.post(
-     '/invoice', function(req, res) {
-         var params = req.body;
-         console.log(params);
-         connection.query('INSERT INTO invoice SET ?', params, function(error, result, fields){
-             if(error) {
-                 res.end(JSON.stringify(error));
-                 throw error;
-             }
-             res.end(JSON.stringify(result));
-         })
-     }
- )
-
- app.get('/user/:id', function (req, res) {
-    connection.query('select * from user where id=?', [req.params.id], function (error, results, fields) {
-       if (error) throw error;
-       res.end(JSON.stringify(results));
-     });
- });
-
- app.get('/asset/:id', function (req, res) {
-    connection.query('select * from user where id=?', [req.params.id], function (error, results, fields) {
-       if (error) throw error;
-       res.end(JSON.stringify(results));
-     });
- });
-
- app.get('/users', function (req, res) {
-    connection.query('select * from user', [req.params.id], function (error, results, fields) {
-       if (error) throw error;
-       res.end(JSON.stringify(results));
-     });
- });
-
- app.get('/asset/user/:id', function (req, res) {
-    connection.query('select * from asset where owner_id=?', [req.params.id], function (error, results, fields) {
-       if (error) throw error;
-       res.end(JSON.stringify(results));
-     });
- });
-
- app.post(
-    '/invoice/update', function(req, res) {
-        var params = req.body;
-        console.log(params);
-        connection.query('UPDATE invoice SET ?', params, function(error, result, fields){
-            if(error) {
-                res.end(JSON.stringify(error));
-                throw error;
-            }
-            res.end(JSON.stringify(result));
-        })
-    }
-)
-
-app.get('/invoice/user/owner/:id', function (req, res) {
-    connection.query('select * from asset where owner_id=?', [req.params.id], function (error, results, fields) {
-       if (error) throw error;
-       res.end(JSON.stringify(results));
-     });
- });
-
- app.get('/invoice/user/tenant/:id', function (req, res) {
-    connection.query('select * from asset where tenant_id=?', [req.params.id], function (error, results, fields) {
-       if (error) throw error;
-       res.end(JSON.stringify(results));
-     });
- });
---
+++ server.js
@@ -0,0 +1,139 @@
+
+var http = require("http");
+var express = require('express');
+var app = express();
+var mysql      = require('mysql');
+var bodyParser = require('body-parser');
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'iamreputed',
+  database : 'test',
+});
+ 
+ 
+connection.connect(function(err) {
+  if (err) throw err
+  console.log('You are now connected with mysql database...')
+})
+
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+
+
+
+var server = app.listen(3000, "127.0.0.1", function () {
+ 
+    var host = server.address().address
+    var port = server.address().port
+   
+    console.log("Example app listening at http://%s:%s", host, port)
+   
+  });
+
+ app.post('/user', function(req, res) {
+
+     var params = req.body;
+     console.log(params);
+     let queryResult;
+     connection.query('INSERT INTO user SET ?', params, function (error, results, fields) {
+         if (error) {
+             res.end(JSON.stringify(error));
+        }
+        
+         queryResult = results;
+     });
+
+     connection.query('SELECT * FROM user where id_proof_number = ?', params.id_proof_number, function (error, results, fields) {
+        if (error) {
+            res.end(JSON.stringify(error));
+       }
+       
+        res.send(results);
+    });
+ })
+
+ app.post('/asset', function(req, res) {
+     var params = req.body;
+     console.log(params);
+     connection.query('INSERT INTO asset SET ?', params, function(error, results, fields) {
+         if (error) {
+             res.end(JSON.stringify(error));
+             throw error;
+         }
+         res.end(JSON.stringify(results));
+     })
+ })
+
+ app.post(
+     '/invoice', function(req, res) {
+         var params = req.body;
+         console.log(params);
+         connection.query('INSERT INTO invoice SET ?', params, function(error, result, fields){
+             if(error) {
+                 res.end(JSON.stringify(error));
+                 throw error;
+             }
+             res.end(JSON.stringify(result));
+         })
+     }
+ )
+
+ app.get('/user/:id', function (req, res) {
+    connection.query('select * from user where id=?', [req.params.id], function (error, results, fields) {
+       if (error) throw error;
+       res.end(JSON.stringify(results));
+     });
+ });
+
+ app.get('/asset/:id', function (req, res) {
+    connection.query('select * from user where id=?', [req.params.id], function (error, results, fields) {
+       if (error) throw error;
+       res.end(JSON.stringify(results));
+     });
+ });
+
+ app.get('/users', function (req, res) {
+    connection.query('select * from user', [req.params.id], function (error, results, fields) {
+       if (error) throw error;
+       res.end(JSON.stringify(results));
+     });
+ });
+
+ app.get('/asset/user/:id', function (req, res) {
+    connection.query('select * from asset where owner_id=?', [req.params.id], function (error, results, fields) {
+       if (error) throw error;
+       res.end(JSON.stringify(results));
+     });
+ });
+
+ app.post(
+    '/invoice/update', function(req, res) {
+        var params = req.body;
+        console.log(params);
+        connection.query('UPDATE invoice SET ?', params, function(error, result, fields){
+            if(error) {
+                res.end(JSON.stringify(error));
+                throw error;
+            }
+            res.end(JSON.stringify(result));
+        })
+    }
+)
+
+app.get('/invoice/user/owner/:id', function (req, res) {
+    connection.query('select * from asset where owner_id=?', [req.params.id], function (error, results, fields) {
+       if (error) throw error;
+       res.end(JSON.stringify(results));
+     });
+ });
+
+ app.get('/invoice/user/tenant/:id', function (req, res) {
+    connection.query('select * from asset where tenant_id=?', [req.params.id], function (error, results, fields) {
+       if (error) throw error;
+       res.end(JSON.stringify(results));
+     });
+ });
